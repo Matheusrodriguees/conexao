4a11f610d4f01343e13c964635cb7e0757dedeea
Matheusrodriguees <84252701+Matheusrodriguees@users.noreply.github.com>
2022-06-07 23:00:43 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,8 @@
+<hr>
+    <div class="form-group">
+        <div class="mx-auto" style="width: 1200px;">
+            <a type="button" href="cadastro.php" class="btn btn-sucess"></a>
+            <a type="button" href="index.php" class="btn btn-default"></a>
+        </div>
+    </div>
+</hr>
